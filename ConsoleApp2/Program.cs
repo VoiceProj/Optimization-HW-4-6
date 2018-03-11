f5e7501f231953af139526509f6b022c5ccaeec3
mazhanoff <mazhanoff@yandex.ru>
2018-03-12 01:37:39 +0300

    Added three points method Added integrity constraints

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,19 +1,20 @@
 ﻿using System;
 using System.Globalization;
 
+
 /// <summary>
 /// Программа для поиска минимума функции методом деления пополам по двум точкам
 /// </summary>
-
-namespace ConsoleApplication1
+/// 
+namespace ConsoleApp2
 {
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Поиск минимума функции:");
-            Console.WriteLine("y=x^2+2*x+4");
-            double a = 0, b = 0, eps = 0;
+            Console.WriteLine("y=x^2+2*x+4");            
+            double a=0, b=0, eps=0;
             try
             {
                 Console.WriteLine("Левая граница:");
@@ -25,36 +26,49 @@ namespace ConsoleApplication1
             }
             catch
             {
-                Console.WriteLine("Неверный формат входных данных");
+                Console.WriteLine("Неверный формат входных данных");                
             }
             Console.WriteLine("----------------------");
             if (a != 0 && b != 0 && eps != 0)
             {
-                double x = 0;
                 int count = 0;
-                while (Math.Abs(b - a) / 2 > eps)
+                double L = b - a;
+                double x2 = (b + a) / 2;
+                while (L > eps)
                 {
                     count++;
-                    x = (a + b) / 2;
-                    double F1 = Y(x - eps);
-                    double F2 = Y(x + eps);
+                    double F2 = Y(x2);
+                    double x1 = a + L / 4;
+                    double x3 = b - L / 4;
+                    double F1 = Y(x1);
+                    double F3 = Y(x3);
                     Console.WriteLine("Итерация #{0}:", count);
-                    Console.WriteLine("a={0}, b={1}", a,b);
-                    Console.WriteLine("F(x-eps)={0}, F(x+eps)={1}", F1, F2);
+                    Console.WriteLine("x1={0}, x2={1}, x3={2}", x1, x2, x3);
+                    Console.WriteLine("F(x1)={0}, F(x2)={1}, F(x3)={2}:", F1, F2, F3);
                     if (F1 < F2)
                     {
-                        b = x;
+                        b = x2;
+                        x2 = x1;
                         Console.WriteLine("Берем левый отрезок");
+                    }
+                    else if (F2 > F3)
+                    {
+                        a = x2;
+                        x2 = x3;
+                        Console.WriteLine("Берем правый отрезок");
                     }
                     else
                     {
-                        a = x;
-                        Console.WriteLine("Берем правый отрезок");
+                        a = x1;
+                        b = x3;
+                        Console.WriteLine("Берем центральный отрезок");
                     }
+                    L = b - a;
+                    Console.WriteLine("----------------------");
                 }
 
-                Console.WriteLine("Минимум функции F(x)={0} и находится в точке {1}", Y(x), x);
-                Console.WriteLine("Количество итераций: {0}", count);
+                Console.WriteLine("Минимум функции F(x)={0} и находится в точке {1}", Y(x2), x2);
+                Console.WriteLine("Количество итераций: {0}", count);              
             }
             Console.ReadLine();
         }
