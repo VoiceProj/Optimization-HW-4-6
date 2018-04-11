86a1e046b6c46becee733cced92677fc7ee76598
mazhanoff <mazhanoff@yandex.ru>
2018-04-11 13:38:46 +0300

    Implement Coordinate Descent method

--- GoldSectionSearch/Program.cs
+++ GoldSectionSearch/Program.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace GoldSectionSearch
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
@@ -31,46 +31,86 @@ namespace GoldSectionSearch
 
             int count = 0;
 
-            double x = goldenSectionSearch(a, b, eps, ref count);
+            double x = goldenSectionSearch(a, b, eps, ref count,true);
 
-            Console.WriteLine("Минимум функции F(x)={0} и находится в точке {1}",Y(x),x);
+            Console.WriteLine("Минимум функции F(x)={0} и находится в точке {1}",Y1(x),x);
             Console.WriteLine("Количество итераций: {0}", count);
             Console.ReadLine();
 
         }
 
-        static double Y(double x)
+        public static double Y1(double x)
         {
-            double y = Math.Pow(x, 2) + 2 * x + 4;
+            double y = Math.Pow((x + 10), 2) + Math.Pow(x, 2);
+            return y;
+        }
+        public static double Y2(double x)
+        {
+            double y = Math.Pow(x, 4);
             return y;
         }
 
-        static double goldenSectionSearch(double l, double r, double eps, ref int count)
+        public static double goldenSectionSearch(double l, double r, double eps, ref int count, bool func)
         {
             double phi = (1 + Math.Sqrt(5)) / 2;
             double resphi = 2 - phi;
             double x1 = l + resphi * (r - l);
             double x2 = r - resphi * (r - l);
-            double f1 = Y(x1);
-            double f2 = Y(x2);
-            do {
-                count++;
-                if (f1 < f2) {
-                    r = x2;
-                    x2 = x1;
-                    f2 = f1;
-                    x1 = l + resphi * (r - l);
-                    f1 = Y(x1);
+
+            if (func)
+            {
+                double f1 = Y1(x1);
+                double f2 = Y1(x2);
+                do
+                {
+                    count++;
+                    if (f1 < f2)
+                    {
+                        r = x2;
+                        x2 = x1;
+                        f2 = f1;
+                        x1 = l + resphi * (r - l);
+                        f1 = Y1(x1);
+                    }
+                    else
+                    {
+                        l = x1;
+                        x1 = x2;
+                        f1 = f2;
+                        x2 = r - resphi * (r - l);
+                        f2 = Y1(x2);
+                    }
                 }
-                else {
-                    l = x1;
-                    x1 = x2;
-                    f1 = f2;
-                    x2 = r - resphi * (r - l);
-                    f2 = Y(x2);
-                }
+                while (Math.Abs(r - l) > eps);
             }
-            while (Math.Abs(r - l) > eps);
+            else
+            {
+                double f1 = Y2(x1);
+                double f2 = Y2(x2);
+                do
+                {
+                    count++;
+                    if (f1 < f2)
+                    {
+                        r = x2;
+                        x2 = x1;
+                        f2 = f1;
+                        x1 = l + resphi * (r - l);
+                        f1 = Y2(x1);
+                    }
+                    else
+                    {
+                        l = x1;
+                        x1 = x2;
+                        f1 = f2;
+                        x2 = r - resphi * (r - l);
+                        f2 = Y2(x2);
+                    }
+                }
+                while (Math.Abs(r - l) > eps);
+            }
+            
+            
             return (x1 + x2) / 2;
         }
     }
